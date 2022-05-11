f3820e8d7d73f1247db3e698407eb7202ff62ef9
Karl Bonnici <karl.bonnici100@gmail.com>
2022-05-11 14:36:16 -0700

    add loop and user input

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 use calculator::eval_expression;
 
+
+fn parse_input() -> String {
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).expect("Must enter an expression");
+    let line = line.trim();
+    line.to_string()
+}
+
 fn main() {
     println!("=== SIMPLE CALCULATOR ===\n");
-    let expr = "5 * 5";
-    println!("{}", eval_expression(expr));
+    loop {
+        let expr = parse_input();
+        if expr.as_str() == "q" {
+            break;
+        }
+        let expr = expr.as_str();
+        println!("{}\n", eval_expression(expr));
+    }
 }
